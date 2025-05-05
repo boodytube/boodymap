ac3d36513280287626958422d98bad6bda3d689a
Abdulrahman Alwakeel <b98576479@gmail.com>
2025-05-05 18:38:59 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+<script>
+    // Wait until the user interacts with the page
+    document.addEventListener('click', function () {
+    const audio = document.getElementById('bg-audio');
+    audio.play();
+    });
+</script>
